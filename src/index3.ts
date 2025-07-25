f782a4def369a76d4afc68b70be60ec5b92ff826
Satyam00915 <satyamupadhyay0495@gmail.com>
2025-07-26 01:06:50 +0530

    Interface and classes

---
+++ src/index3.ts
@@ -0,0 +1,41 @@
+let userDetail = {
+  name: "satyam",
+  age: 21,
+  address: {
+    city: "vizag",
+    country: "india",
+    pincode: 531021,
+  },
+};
+
+let userDetail2 = {
+  name: "ashborne",
+  age: 155,
+};
+
+interface userDetailType {
+  name: string;
+  age: number;
+  address?: Address;
+}
+
+interface Office {
+  address: Address;
+}
+
+interface Address {
+  city: string;
+  country: string;
+  pincode: number;
+}
+
+function isLegal(user: userDetailType): boolean {
+  if (user.age > 18) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(isLegal(userDetail));
+console.log(isLegal(userDetail2));
---
+++ src/index4.ts
@@ -0,0 +1,31 @@
+interface People {
+  name: string;
+  age: number;
+  isLegal(): boolean;
+}
+
+class Manager implements People {
+  name: string;
+  age: number;
+  number: string;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+    this.number = "adasd";
+  }
+
+  isLegal(): boolean {
+    if (this.age > 18) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+let user1 = new Manager("Satyam", 20);
+
+console.log(user1.name + " " + user1.age + " " + user1.number);
+
+
